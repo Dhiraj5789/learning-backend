00cfdf0e4726932686cc93846ead39791f19a6cc
Dhiraj Suthar <dhirajsuthar5789@gmail.com>
2024-01-18 10:30:17 +0530

    express middlewares

--- working-with-expressjs/app.js
+++ working-with-expressjs/app.js
@@ -4,6 +4,19 @@ const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  console.log("Always Rujnning");
+  next();
+});
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  console.log("In Add Product middleware");
+  res.send("<h1>Add Product Page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
